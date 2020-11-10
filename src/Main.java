b7d1e3f6126ad2895179b53a84db6442f421a786
kamalkablan <kimokablan10@gmail.com>
2020-11-10 12:54:24 -0500

    for loop

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,15 @@ public class Main {
     public static void main(String[] args) {
         int number = 0;
         int number2 = 3;
-        if (number < number2){
-            System.out.println("Hello world");
-        }else {
-            System.out.println("let me here oh yeahhhhh");
+        int j = 5;
+        for (int i =0; i >= j; i++){
+            if (number < number2){
+                System.out.println("Hello world");
+            }else {
+                System.out.println("let me here oh yeahhhhh");
+            }
         }
+
 
 
     }
